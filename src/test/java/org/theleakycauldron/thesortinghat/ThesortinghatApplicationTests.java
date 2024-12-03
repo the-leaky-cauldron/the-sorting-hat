60671bbff57e0ee599b80780cf51b97655736beb
mandalavijaysurya <mandala.vijay.surya@gmail.com>
2024-12-03 17:01:17 +0530

    feat: Added the change password feature - Implemented the basic structure of how the routes work - Two routes are introduced - GET password: which generates the link and send to client via owlery   service. - POST password: which changes the password from the backend of   frontend.

--- src/test/java/org/theleakycauldron/thesortinghat/ThesortinghatApplicationTests.java
+++ src/test/java/org/theleakycauldron/thesortinghat/ThesortinghatApplicationTests.java
@@ -8,6 +8,7 @@ class ThesortinghatApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
---
+++ src/test/java/org/theleakycauldron/thesortinghat/controllers/SortingHatControllerTest.java
@@ -0,0 +1,24 @@
+package org.theleakycauldron.thesortinghat.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SortingHatControllerTest {
+
+    @Test
+    void registration() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void generatePasswordLink() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+}
